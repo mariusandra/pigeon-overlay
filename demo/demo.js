8c8625b30e18f49250a6ef89ba16cf86b1898e71
Marius Andra <marius.andra@gmail.com>
2019-01-16 08:07:11 +0100

    boxShadow in demo

--- demo/demo.js
+++ demo/demo.js
@@ -13,8 +13,8 @@ export default class Demo extends Component {
              zoom={12}
              width={600}
              height={400}>
-          <Overlay anchor={[50.879, 4.6997]} offset={[60, 39]} style={{border: '1px solid red'}}>
-            <img src={pigeon} width={120} height={79} alt='' />
+          <Overlay anchor={[50.879, 4.6997]} offset={[60, 39]} style={{ boxShadow: '3px 3px 5px rgba(0,0,0,0.5)' }}>
+            <img src={pigeon} width={120} height={79} alt='' style={{ display: 'block' }} />
           </Overlay>
         </Map>
       </div>
